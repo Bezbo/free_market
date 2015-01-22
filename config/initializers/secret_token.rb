5f24472bc4a5715e6cb94ea3b862dc2fa9d358dd
Bezbo <without.a.beard@gmail.com>
2015-01-22 16:31:57 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreeMarket::Application.config.secret_key_base = 'dadcb12a352e0e9df97f4dea985e4ddb35e519bb225713bb0a4516a7bca516b7148a32a49d849d4ab86fa6738624b38c6de74ace23e18e4c4f6c9b4e386a42b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FreeMarket::Application.config.session_store :cookie_store, key: '_free_market_session'
