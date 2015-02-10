52bf8cd63da09c8b8fae53271894bccac550c13d
Bezbo <without.a.beard@gmail.com>
2015-02-10 16:00:31 +0300

    basic user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,62 @@
-class UsersController < ApplicationController
+class UsersController < Clearance::UsersController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(permit_params)
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(permit_params)
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
+  end
+
+  private
+
+  def permit_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def user_from_params
+    user_params = params[:user] || Hash.new
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    name = user_params.delete(:name)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+      user.name = name
+    end
+  end
+
 end
