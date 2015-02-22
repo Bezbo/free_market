85dc6cb6854d1f5e1f2bec1300be7a72756e0b58
Bezbo <without.a.beard@gmail.com>
2015-02-22 13:40:33 +0300

    validations for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 75 }
+  validates :description, length: { maximum: 1000 }
+  validates :user_id, presence: true
 end
